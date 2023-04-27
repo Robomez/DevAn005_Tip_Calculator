3ca5935a0aea35bb3c272fa14ecaaa426d89336e
Alex <108117314+Robomez@users.noreply.github.com>
2023-04-28 01:22:42 +0300

    Added icon, drawables, styles, themes (day/night) Scrollable layout Hide keyboard on enter press

--- app/src/main/java/com/example/devan005_tip_calculator/MainActivity.kt
+++ app/src/main/java/com/example/devan005_tip_calculator/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.devan005_tip_calculator
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import com.example.devan005_tip_calculator.databinding.ActivityMainBinding
 import java.text.NumberFormat
 
@@ -14,11 +18,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.calculateButton.setOnClickListener { calculateTip() }
+        binding.costOfServiceEditText.setOnKeyListener { view, keyCode, _ -> handleKeyEvent(view, keyCode) }
     }
 
     private fun calculateTip() {
-        val cost = binding.costOfService.text.toString().toDoubleOrNull()
-        if (cost == null ) {
+        val cost = binding.costOfServiceEditText.text.toString().toDoubleOrNull()
+        if (cost == null) {
             displayTip(0.0)
             return
         }
@@ -35,8 +40,19 @@ class MainActivity : AppCompatActivity() {
         displayTip(tip)
     }
 
-    private fun displayTip(tip : Double) {
+    private fun displayTip(tip: Double) {
         val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
         binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
+    }
+
+    private fun handleKeyEvent(view: View, keyCode: Int): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_ENTER) {
+            // Hide the keyboard
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            return true
+        }
+        return false
     }
 }
